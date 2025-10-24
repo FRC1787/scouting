6e919b3bae68022f75e6d3e7a1a2a0f3833e2995
Orange Robotics <orangerobotics@gmail.com>
2025-10-24 13:54:21 -0400

    Added some more

--- tba_match_sorting.py
+++ tba_match_sorting.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 import os
 import json
+import data_process
 
 tbaFileName = 'TBAMatches.json'
 
@@ -7,7 +9,35 @@ global tbaMatchesWithPlayoffs
 global tbaQualsMatches
 tbaQualsMatches = []
 
+@dataclass
+class singleAllianceMatch:
+    matchNum: int = 0
+    isRedAlliance: bool = False
+    firstBotTeamNumber: int = 0
+    secondBotTeamNumber: int = 0
+    thirdBotTeamNumber: int = 0
 
+    autol4Count: int = 0
+    autol3Count: int = 0
+    autol2Count: int = 0
+    autol1Count: int = 0
+    autoMove: bool = False
+
+    l4Count: int = 0
+    l3Count: int = 0
+    l2Count: int = 0
+    l1Count: int = 0
+    proccesorCount: int = 0
+    netCount: int = 0
+
+    # end game can be None, Parked, ShallowCage, DeepCage
+    firstBotEndgame: str = "None"
+    secondBotEndgame: str = "None"
+    thirdBotEndgame: str = "None"
+
+def makeBothAllianceMatchClass(SingleTeamSingleMatchEntry):
+    print(SingleTeamSingleMatchEntry)
+        
 try:
     with open(tbaFileName, 'r') as file:
         tbaMatchesWithPlayoffs = json.load(file)
@@ -23,3 +53,4 @@ for i, match in enumerate(tbaMatchesWithPlayoffs):
     #print(match["comp_level"])
 
 #print(tbaQualsMatches[11]["match_number"])
+
